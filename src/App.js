ab561bde3be63da62c83c48c1ea7b120e17ca020
yunchan123 <phenomenal312@hufs.ac.kr>
2024-04-14 18:22:52 +0900

    fix: css outline

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
   ]);
   return (
     <div className="w-[100vw] h-[100vh] flex justify-center">
-      <div className="w-[500px]">
+      <div className="w-[500px] border-2 border-black ">
         <RouterProvider router={router} />
       </div>
     </div>
